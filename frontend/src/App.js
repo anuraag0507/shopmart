974a9bef9a2e396c11e7f5da67e27894f402adb6
anuraag0507 <93443023+anuraag0507@users.noreply.github.com>
2023-07-30 23:43:20 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,6 +26,7 @@ function App() {
         <Header />
         <main className="pt-16 bg-slate-100 min-h-[calc(100vh)]">
           <Outlet />
+         <Footer />
         </main>
       </div>
     </>
