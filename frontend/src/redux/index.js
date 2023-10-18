f21260d1837f5ad0753ffdc898ff4a71cc4ab25a
anuraag0507 <93443023+anuraag0507@users.noreply.github.com>
2023-10-18 23:45:17 +0530

    Update index.js

--- frontend/src/redux/index.js
+++ frontend/src/redux/index.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSliceReducer from "./userSlice";
 import productSlideReducer from "./productSlide";
 
-export const store = configureStore ({
+export const store = configureStore({ 
   reducer: {
     user : userSliceReducer,
     product : productSlideReducer
